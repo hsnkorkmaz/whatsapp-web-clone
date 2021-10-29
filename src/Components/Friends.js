61b71e9665a22b4e002cb091daf7d2c8250aae33
Hasan Mustafa <hasankorkmaz-@hotmail.com>
2021-10-29 13:37:30 +0200

    changed the conversations

--- src/Components/Friends.js
+++ src/Components/Friends.js
@@ -7,7 +7,7 @@ export const friendsList = [
         Messages: [
             {
                 Type: "Sent",
-                Message: "Hello",
+                Message: "Hello, welcome to “business name” thank you for visiting our website. How can we assist you?",
                 Time: new Date(2021, 10, 28, 12, 0, 0, 0),
             },
             {
@@ -17,7 +17,12 @@ export const friendsList = [
             },
             {
                 Type: "Sent",
-                Message: "I am fine",
+                Message: "Hello, thank you for using “business name” service. Can I help you with something?",
+                Time: new Date(2021, 10, 28, 12, 10, 0, 0),
+            },
+            {
+                Type: "Sent",
+                Message: "Wuhuu",
                 Time: new Date(2021, 10, 28, 12, 10, 0, 0),
             }
         ],
@@ -28,7 +33,7 @@ export const friendsList = [
         Messages: [
             {
                 Type: "Sent",
-                Message: "Hello",
+                Message: "Hello, “customer name” welcome back to “ business name”. Did you face any further problem with the “earlier issue”?",
                 Time: new Date(2021, 10, 28, 12, 0, 0, 0),
             },
             {
@@ -38,7 +43,12 @@ export const friendsList = [
             },
             {
                 Type: "Sent",
-                Message: "I am fine",
+                Message: "Welcome back “customer name”. Happy to see you again. What can we do for you today?",
+                Time: new Date(2021, 10, 28, 12, 10, 0, 0),
+            },
+            {
+                Type: "Sent",
+                Message: "Bye",
                 Time: new Date(2021, 10, 28, 12, 10, 0, 0),
             }
         ],
@@ -49,7 +59,7 @@ export const friendsList = [
         Messages: [
             {
                 Type: "Sent",
-                Message: "Hello",
+                Message: "Hi, “customer name”. May I request you to share your contact details?",
                 Time: new Date(2021, 10, 28, 12, 0, 0, 0),
             },
             {
@@ -59,7 +69,12 @@ export const friendsList = [
             },
             {
                 Type: "Sent",
-                Message: "wohooo",
+                Message: "Hi, “customer name”. Would you like to know more about the subscriptions for “product or service name”? If you want I can detail you.",
+                Time: new Date(2021, 10, 28, 12, 10, 0, 0),
+            },
+            {
+                Type: "Sent",
+                Message: "See you",
                 Time: new Date(2021, 10, 28, 12, 10, 0, 0),
             }
         ],
@@ -70,7 +85,7 @@ export const friendsList = [
         Messages: [
             {
                 Type: "Sent",
-                Message: "Hello",
+                Message: "Hello! “customer name”, What brings you to our website? What are you looking for? It will help us to serve you better.",
                 Time: new Date(2021, 10, 28, 12, 0, 0, 0),
             },
             {
@@ -80,7 +95,12 @@ export const friendsList = [
             },
             {
                 Type: "Sent",
-                Message: "No way!",
+                Message: "Good morning “customer name”, What brings you to our site? What was the source you came to know about it?",
+                Time: new Date(2021, 10, 28, 12, 10, 0, 0),
+            },
+            {
+                Type: "Sent",
+                Message: "Okay",
                 Time: new Date(2021, 10, 28, 12, 10, 0, 0),
             }
         ],
